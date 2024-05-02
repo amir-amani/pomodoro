4d31598e7f9817cc5e7eba1b5ce8a22ed47c2fe9
Amir M. Amani <amani.1381.amir@gmail.com>
2024-05-02 17:09:30 +0430

    fixed the vit.congig

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://amir-amani.github.io/pomodoro/",
   plugins: [react()],
 })
